7df31f205f3758aecfcadd9ac84361348c4e8f4a
diegoandcontroll <lukasxdp2@gmail.com>
2023-04-05 00:27:03 -0300

    feat: create modulo jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signIn(email: string, pass: string): Promise<any> {
+    const user = await this.userService.findEmail(email);
+    const userWithPass = await this.userService.findById(user.id);
+    if (!userWithPass) {
+      throw new NotFoundException('User not found');
+    }
+    const hashByPass = await bcrypt.compare(pass, userWithPass.password);
+    if (!hashByPass) {
+      throw new UnauthorizedException('Email or password incorrect');
+    }
+    const payload = { username: user.email, sub: user.id };
+    const token = await this.jwtService.signAsync(payload);
+    return {
+      user,
+      access_token: token,
+    };
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,62 @@
-import { Injectable } from '@nestjs/common';
-
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { IResponseUser } from 'src/types';
+import * as bcrypt from 'bcrypt';
+import { User } from '@prisma/client';
 @Injectable()
 export class UsersService {
-  findAll() {
-    return `This action returns all users`;
+  constructor(
+    @Inject(PrismaService)
+    private readonly prisma: PrismaService,
+  ) {}
+  async create(data: CreateUserDto): Promise<IResponseUser> {
+    const hash = await bcrypt.hash(data.password, 10);
+    data.password = hash;
+    const user = await this.prisma.user.create({
+      data: {
+        email: data.email,
+        password: data.password,
+      },
+    });
+    return user;
+  }
+  async findById(id: string): Promise<User> {
+    const user = await this.prisma.user.findFirst({
+      where: { id },
+    });
+    if (!user) {
+      throw new HttpException('User not found!', HttpStatus.NOT_FOUND);
+    }
+    return user;
+  }
+  async find(): Promise<IResponseUser[]> {
+    const users = await this.prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    return users;
+  }
+
+  async findEmail(email: string): Promise<IResponseUser> {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true,
+      },
+    });
+    if (!user) {
+      throw new HttpException('User not found!', HttpStatus.NOT_FOUND);
+    }
+    return user;
   }
 }
